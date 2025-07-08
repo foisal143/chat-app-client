948b5393493f8e336065bb374ff3a08329e45e9c
Md Foisal Islam <islammdfoisal54@gmail.com>
2025-07-09 00:04:47 +0600

    change the server url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-server-bu2s.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
